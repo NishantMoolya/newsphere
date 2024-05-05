faa57194b0ba6f4aaeae84e77d9daf1bcd6a989e
NishantMoolya <nishantmoolya@gmail.com>
2024-05-05 23:28:43 +0530

    changed api key

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ country, setCountry, setSearch }) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
-                <NavLink className="navbar-brand" href="/" style={{ fontWeight: '700' }}>Newsphere</NavLink>
+                <NavLink className="navbar-brand" href="/" style={{ fontWeight: '700' }}>Nishnews</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
